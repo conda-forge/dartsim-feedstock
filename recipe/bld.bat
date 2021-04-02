c9e08d2790518ee8b90f99955b9497ef75a73b59
Silvio Traversaro <silvio@traversaro.it>
2021-04-02 22:07:24 +0200

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -6,6 +6,7 @@ cmake -G "Ninja" ^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
     -DDART_MSVC_DEFAULT_OPTIONS=ON ^
     -DDART_VERBOSE=ON ^
+    -DBUILD_TESTING:BOOL=ON ^
     %SRC_DIR%
 
 echo "Print CMakeOutput:"
@@ -18,4 +19,7 @@ ninja
 if errorlevel 1 exit 1
 
 ninja install
+if errorlevel 1 exit 1
+
+ctest --output-on-failure
 if errorlevel 1 exit 1
