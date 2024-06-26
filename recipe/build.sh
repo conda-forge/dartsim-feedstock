dcbac4b0db419e41eb7700a8b462c8cb66315550
Jeongseok Lee <jslee02@gmail.com>
2024-06-26 06:37:34 -0700

    Disable building with -march=native

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,15 +8,6 @@ else
   NUM_PARALLEL=-j${CPU_COUNT}
 fi
 
-# Disable SIMD for linux-ppc64le, linux_aarch64, and any OSX platform; enable for other platforms
-if [[ "${target_platform}" == "linux-ppc64le" ]] ||
-  [[ "${target_platform}" == "linux-aarch64" ]] ||
-  [[ "${target_platform}" == osx-* ]]; then
-  DART_ENABLE_SIMD=OFF
-else
-  DART_ENABLE_SIMD=ON
-fi
-
 if [[ "${target_platform}" == osx-* ]]; then
   # See https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
   CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
@@ -29,7 +20,7 @@ cmake ${CMAKE_ARGS} $SRC_DIR \
   -DCMAKE_INSTALL_LIBDIR=lib \
   -DDART_VERBOSE:BOOL=ON \
   -DDART_TREAT_WARNINGS_AS_ERRORS:BOOL=OFF \
-  -DDART_ENABLE_SIMD:BOOL=${DART_ENABLE_SIMD} \
+  -DDART_ENABLE_SIMD:BOOL=OFF \
   -DDART_BUILD_DARTPY:BOOL=OFF \
   -DDART_USE_SYSTEM_IMGUI:BOOL=ON
 
