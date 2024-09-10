eb835e769c7128bf24256743a02fcc111490152e
Jeongseok Lee <jslee02@gmail.com>
2024-09-10 10:09:22 -0700

    Set max CPU count to 2

--- recipe/bld_py.bat
+++ recipe/bld_py.bat
@@ -1,6 +1,6 @@
 :: Check the number of cores to use by ninja by default
-IF %CPU_COUNT% GTR 4 (
-    set CMAKE_BUILD_PARALLEL_LEVEL=4
+IF %CPU_COUNT% GTR 2 (
+    set CMAKE_BUILD_PARALLEL_LEVEL=2
 ) ELSE (
     set CMAKE_BUILD_PARALLEL_LEVEL=%CPU_COUNT%
 )
