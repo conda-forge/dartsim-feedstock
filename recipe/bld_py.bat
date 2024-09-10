ce0cd93de555f831e78b725fa390dd77f20f6313
Jeongseok Lee <jslee02@gmail.com>
2024-09-10 13:15:22 -0700

    Set CPU count to 1

--- recipe/bld_py.bat
+++ recipe/bld_py.bat
@@ -1,9 +1,4 @@
-:: Check the number of cores to use by ninja by default
-IF %CPU_COUNT% GTR 2 (
-    set CMAKE_BUILD_PARALLEL_LEVEL=2
-) ELSE (
-    set CMAKE_BUILD_PARALLEL_LEVEL=%CPU_COUNT%
-)
+set CMAKE_BUILD_PARALLEL_LEVEL=1
 
 :: Install the Python package
 python -m pip install . -vv
