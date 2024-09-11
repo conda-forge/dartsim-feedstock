67236877fe63d4e4220577b8a2d2cc3a343912ce
Jeongseok Lee <jslee02@gmail.com>
2024-09-10 17:54:47 -0700

    Attempt to resolve OOM

--- recipe/bld_py.bat
+++ recipe/bld_py.bat
@@ -1,4 +1,8 @@
+:: Limit parallel jobs to 1 to reduce memory usage
 set CMAKE_BUILD_PARALLEL_LEVEL=1
+
+:: Add compiler flags to increase precompiled header memory and optimize inline functions
+set CXXFLAGS=%CXXFLAGS% /Zm200 /Zc:inline
 
 :: Install the Python package
 python -m pip install . -vv
