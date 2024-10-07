940a6f516d6a6f4d16792303834f7d1c2fbacfe5
Jeongseok (JS) Lee <jslee02@users.noreply.github.com>
2024-10-07 11:35:40 -0700

    Update bld_py.bat

--- recipe/bld_py.bat
+++ recipe/bld_py.bat
@@ -1,5 +1,5 @@
 :: Limit parallel jobs to 1 to reduce memory usage
-set CMAKE_BUILD_PARALLEL_LEVEL=2
+set CMAKE_BUILD_PARALLEL_LEVEL=1
 
 :: Add compiler flags to increase precompiled header memory and optimize inline functions
 set CXXFLAGS=%CXXFLAGS% /Zm800 /Zc:inline
