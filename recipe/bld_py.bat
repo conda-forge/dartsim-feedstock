157dc99e45b74f921d6bb202fec53b9b3c90abd6
Jeongseok Lee <jslee02@gmail.com>
2024-12-21 12:22:14 -0800

    Zm1500

--- recipe/bld_py.bat
+++ recipe/bld_py.bat
@@ -5,7 +5,8 @@ set CMAKE_BUILD_PARALLEL_LEVEL=1
 ::   /Zm1600  increases compiler memory limit
 ::   /bigobj  allows larger object files
 ::   /Zc:inline  controls inline function expansion
-set CXXFLAGS=%CXXFLAGS% /Zm3200 /bigobj /Zc:inline /Z7 /O1
+set CXXFLAGS=%CXXFLAGS% /Zm1500 /bigobj /Zc:inline /Z7 /O1
 
 :: Install the Python package (which will invoke CMake under the hood)
 python -m pip install . -vv
+if errorlevel 1 exit 1
