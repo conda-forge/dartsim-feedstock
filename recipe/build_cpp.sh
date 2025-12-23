1a6200b33f46d247d99f049bb66619120e6d5759
Jeongseok Lee <jslee02@gmail.com>
2025-12-23 06:03:12 -0800

    Speed up ppc64le builds

--- recipe/build_cpp.sh
+++ recipe/build_cpp.sh
@@ -3,7 +3,7 @@
 mkdir build && cd build
 
 if [[ ${target_platform} == "linux-ppc64le" ]]; then
-  NUM_PARALLEL=-j1
+  NUM_PARALLEL=-j2
 else
   if [[ ${CPU_COUNT} -gt 8 ]]; then
     NUM_PARALLEL=-j8
