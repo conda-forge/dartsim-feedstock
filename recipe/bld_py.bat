29c43ea56768f964ff22112fef9556c572092769
Jeongseok Lee <jslee02@gmail.com>
2025-12-22 17:57:43 -0800

    Use limited /MP for dartpy build

--- recipe/bld_py.bat
+++ recipe/bld_py.bat
@@ -7,9 +7,6 @@ if exist build rmdir /s /q build
 :: Use a build temp directory on the system drive to avoid D: disk exhaustion
 set "DARTPY_BUILD_TEMP=%USERPROFILE%\\AppData\\Local\\Temp\\dartpy_build"
 
-:: Use Ninja to speed up MSVC builds
-set "CMAKE_GENERATOR=Ninja"
-
 :: Avoid MSVC release flags that add /Zi and /GL (big memory spikes)
 set "CMAKE_ARGS=%CMAKE_ARGS% -DDART_MSVC_DEFAULT_OPTIONS=ON"
 
@@ -19,7 +16,7 @@ set "CMAKE_ARGS=%CMAKE_ARGS% -DDART_MSVC_DEFAULT_OPTIONS=ON"
 :: - /bigobj     => allow larger .obj files
 :: - (NO /Z7)    => no debug symbols
 :: - (NO /GL)    => ensure LTCG is off
-set CXXFLAGS=%CXXFLAGS% /Od /Zm1500 /bigobj
+set CXXFLAGS=%CXXFLAGS% /Od /Zm1500 /bigobj /MP2
 
 :: Install the Python package (triggers CMake build via pip)
 python -m pip install . -vv
